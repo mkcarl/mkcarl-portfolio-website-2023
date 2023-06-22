d5cb1336a41455a78b11ab44842e120fb7161fb8
munky <munky29@gmail.com>
2023-06-22 23:09:33 +0800

    mobile design done

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
             scrollingSpeed={1000}
             navigation={true}
             navigationPosition={"right"}
-            normalScrollElements={"#about-paragraph"}
+            normalScrollElements={"#about-paragraph, .project-description"}
             render={({state, fullpageApi}) => {
                 return(
                     <ReactFullpage.Wrapper>
--- src/projects/Project.jsx
+++ src/projects/Project.jsx
@@ -1,23 +1,22 @@
 import {Tooltip} from "react-tooltip";
-import React from "react";
 
 function Project({title, description, image, github, skills, visible, reversed}){
     return (
         <div className={`h-screen w-screen ${visible ? 'hidden': ''} flex justify-center`}>
-            <div className={`flex ${reversed ? 'flex-row-reverse' : 'flex-row'} `}>
-                <div className={'h-full w-1/2 flex items-center justify-center basis-1/2'}>
+            <div className={`flex flex-col ${reversed ? 'lg:flex-row-reverse' : 'lg:flex-row'} justify-center`}>
+                <div className={'w-full lg:w-1/2 flex items-center justify-center'}>
                     <img src={image} alt={title} className={'w-3/5 h-auto rounded-lg '}/>
 
                 </div>
-                <div className={'flex justify-center flex-col basis-1/2 p-16 text-justify'}>
-                    <h1 className={'text-7xl font-semibold'}>{title}</h1>
-                    <h2 className={'text-2xl'}><a className={'link'} target={"_blank"} href={github}>{title}</a> is {description}</h2>
-                    <div className={'flex gap-8 p-4'}>
+                <div className={'flex justify-center flex-col lg:basis-1/2 p-12 lg:px-16 text-justify'}>
+                    <h1 className={'text-3xl font-semibold'}>{title}</h1>
+                    <h2 id className={'project-description text-xl max-h-48 overflow-scroll lg:overflow-auto lg:max-h-max'}><a className={'link'} target={"_blank"} rel={'noreferrer'} href={github}>{title}</a> is {description}</h2>
+                    <div className={'flex flex-wrap gap-4 lg:gap-8 p-4'} >
                         {
                             Object.entries(skills).map(([skillName, skillImage]) => {
                                 return (
                                     <>
-                                        <img src={skillImage} alt={skillName} className={'w-16 h-16'} id={`${skillName.toLowerCase()}`}/>
+                                        <img src={skillImage} alt={skillName} className={'w-12 lg:w-16 h-12 lg:h-16'} id={`${skillName.toLowerCase()}`}/>
 
                                         <Tooltip
                                             anchorSelect={`#${skillName.toLowerCase()}`}
